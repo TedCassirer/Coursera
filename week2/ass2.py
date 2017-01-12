ecf34ec9835d4795cae30744897215914cceb3e9
Ted Cassirer <ted.cassirer@aphelion.se>
2017-01-12 14:30:35 +0100

    Week2 for Datascience python

---
+++ week2/ass2.py
@@ -0,0 +1,63 @@
+import pandas as pd
+
+df = pd.read_csv('olympics.csv', index_col=0, skiprows=1)
+
+for col in df.columns:
+    if col[:2] == '01':
+        df.rename(columns={col: 'Gold' + col[4:]}, inplace=True)
+    if col[:2] == '02':
+        df.rename(columns={col: 'Silver' + col[4:]}, inplace=True)
+    if col[:2] == '03':
+        df.rename(columns={col: 'Bronze' + col[4:]}, inplace=True)
+    if col[:1] == 'N':
+        df.rename(columns={col: '#' + col[1:]}, inplace=True)
+
+names_ids = df.index.str.split('\s\(')  # split the index by '('
+
+df.index = names_ids.str[0]  # the [0] element is the country name (new index)
+df['ID'] = names_ids.str[1].str[:3]  # the [1] element is the abbreviation or ID (take first 3 characters from that)
+
+df = df.drop('Totals')
+df.head()
+
+# Country with most gold medals
+def q1():
+    return df['Gold'].idxmax()
+
+#Biggest difference between Summer Gold and Winter Gold
+def q2():
+    g = df['Gold']
+    g1 = df['Gold.1']
+    return abs(g-g1).idxmax()
+
+#q2 but relative to total gold
+def q3():
+    has_golds = df[(df['Gold'] > 0) & (df['Gold.1'] > 0)]
+    g = has_golds['Gold']
+    g1 = has_golds['Gold.1']
+    return (abs(g-g1)/(g+g1)).idxmax()
+
+def q4():  
+    g2 = df['Gold.2']
+    s2 = df['Silver.2']
+    b2 = df['Bronze.2']
+    return pd.Series(g2*3 + s2*2 + b2, name='Points')
+
+census_df = pd.read_csv('census.csv',  skiprows=0)
+
+def q5():
+    counties = census_df[census_df.SUMLEV == 50]
+    res = pd.value_counts(counties.STNAME.values, sort=True)
+    return res.idxmax()
+
+def q6():
+    counties = census_df[census_df.SUMLEV == 50]
+    largestCounties = counties.groupby('STNAME')['CENSUS2010POP'].nlargest(3)
+    
+
+g = df['Gold']
+g1 = df['Gold.1']
+
+g2 = df['Gold.2']
+s2 = df['Silver.2']
+b2 = df['Bronze.2']
